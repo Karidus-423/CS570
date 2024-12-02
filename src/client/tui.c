590545bd13be24a8699b28b8fc752edab3f286b4
Karidus-423 <kapuerto23@gmail.com>
2024-12-02 14:57:35 -0500

    ncurses windows initial setup

--- src/client/tui.c
+++ src/client/tui.c
@@ -1,3 +1,5 @@
+#include "curses.h"
+#include "ncurses.h"
 #include "tuiclnt.h"
 
 void FileServer(char* usr_name, char *host, CLIENT **client) {
@@ -5,21 +7,58 @@ void FileServer(char* usr_name, char *host, CLIENT **client) {
     strcpy(server_dir, usr_name);
     strcat(server_dir, "@");
     strcat(server_dir, host);
+    WINDOW *main_win;
+	int cy,cx;
+    Vec2 win_size = {80, 30};
+
+    Win menu_opt, cont_opt, search_opt, list_opt;
+    menu_opt.id = 1;
+    menu_opt.pos = (Vec2){55, 3};
+    menu_opt.size = (Vec2){55, 3};
+
+    cont_opt.id = 1;
+    cont_opt.pos = (Vec2){55, 4};
+    cont_opt.size = (Vec2){55, 24};
 
     // -----------------------------BEGIN TUI-----------------------------------
-	initscr(); noecho(); curs_set(0);
+    initscr();
+    cbreak();
+    noecho();
 
-    int yMax, xMax;
+	//Center
+	getmaxyx(stdscr,cy,cx);
+    int starty = (cy - win_size.y) / 2;
+    int startx = (cx - win_size.x) / 2;
 
-    getmaxyx(stdscr, yMax, xMax);
+    main_win = newwin(win_size.y, win_size.x, starty, startx);
+    box(main_win, 0, 0);
+    mvwprintw(main_win, 0, 2, server_dir);
 
-    WINDOW *win = newwin(yMax / 2, xMax / 2, yMax / 4, xMax / 4);
-    box(win, 0, 0);
-    mvwprintw(win, 0, 2, "%s", server_dir);
-    mvwprintw(win, 2, 2, "> ");
-    while (wgetch(win) != 'q') {
-    };
+	//Menu
+	WINDOW* menu = subwin(main_win, menu_opt.size.y, menu_opt.size.x,
+					      starty + 2, startx + 2);
+    box(menu, 0, 0);
+    mvwprintw(menu, 0, 2, "MENU");
+	//Content
+	WINDOW* content = subwin(main_win, cont_opt.size.y, cont_opt.size.x, 
+					        starty + 5, startx + 2);                    
+    box(content, 0, 0);
+    mvwprintw(content, 0, 2, "CONTENT");
+	//List
+	WINDOW* list = subwin(main_win, 27, 20, 
+					        starty + 2, startx + 58);                    
+    box(list, 0, 0);
+    mvwprintw(list, 0, 2, "List");
+
+    wrefresh(menu);
+    wrefresh(content);
+    wgetch(main_win);
+
+    delwin(menu);
+    delwin(content);
     endwin();
-    // ------------------------------END TUI------------------------------------ 
-	return;
+
+    // ------------------------------END TUI------------------------------------
+    return;
 }
+
--- src/client/tuiclnt.h
+++ src/client/tuiclnt.h
@@ -12,6 +12,17 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct Vec2 {
+  int x;
+  int y;
+} Vec2;
+
+typedef struct Win {
+  Vec2 pos;
+  Vec2 size;
+  int id;
+} Win;
+
 int Open(char *filename_to_open, CLIENT **clnt, char *usr_name);
 int Write(int fd, char *bfr, CLIENT **clnt, int bytes, char *usr_name);
 void Close(int fd, CLIENT **clnt);
@@ -19,6 +30,8 @@ int Read(int fd, CLIENT **clnt, char *bfr, int numbytes, char *usr_name);
 int Seek(int fd, int offset, CLIENT **clnt, char *usr_name);
 void Delete(char *file_name, char *usr_name, CLIENT **clnt);
 void List(char *usr_name, CLIENT **clnt);
+
 void FileServer(char *usr_name, char *host, CLIENT **client);
+void HandleMenu();
 
 #endif
